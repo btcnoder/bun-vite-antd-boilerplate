e346be450d4aff068f61aaaac580277792ac63ba
btcnoder <btcnoder@gmail.com>
2024-03-10 15:17:10 +0800

    fix: add demo page

---
+++ src/pages/demo.tsx
@@ -0,0 +1,36 @@
+import { Button } from 'antd';
+export default function Demo() {
+  return (
+    <>
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <button
+        type="button"
+        className="mb-2 me-2 rounded-full  bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Default
+      </button>
+      <button
+        type="button"
+        className="mb-2 me-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+      >
+        Alternative
+      </button>
+      <button
+        type="button"
+        className="mb-2 me-2 rounded-full bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+      >
+        Dark
+      </button>
+      <button
+        type="button"
+        className="mb-2 me-2 rounded-full border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+      >
+        Light
+      </button>
+    </>
+  );
+}
